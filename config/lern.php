c2b0957d84a5d23018b4d3fdaa2a4e502308910f
Tyler Arbon <tylercd100@gmail.com>
2018-02-16 16:12:19 -0700

    Added new connection property to the config

--- config/lern.php
+++ config/lern.php
@@ -14,8 +14,19 @@ return [
          */
         'model' => \Tylercd100\LERN\Models\ExceptionModel::class,
 
+        /**
+         * Database connection to use. Null is the default connection.
+         */
+        'connection'=>null,
+
+        /**
+         * Database table to use
+         */
         'table'=>'vendor_tylercd100_lern_exceptions',
         
+        /**
+         * Information to store
+         */
         'collect'=>[
             'method'=>false, //When true it will collect GET, POST, DELETE, PUT, etc...
             'data'=>false, //When true it will collect Input data
